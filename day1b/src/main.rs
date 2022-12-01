5a2038957c4fe311e3b4e9036feacf10aeca8d72
mathijs-bakker <mathijs_bakker@me.com>
2022-12-01 15:32:33 +0100

    Refactor

--- day1b/src/main.rs
+++ day1b/src/main.rs
@@ -1,34 +1,10 @@
 fn main() {
-    let input = include_str!("../input.txt")
-        .lines()
-        .map(|n| n.parse::<u32>().unwrap_or(0))
+    let mut calories = include_str!("../input.txt")
+        .split("\n\n")
+        .map(|e| e.lines().map(|c| c.parse::<u32>().unwrap()).sum())
         .collect::<Vec<u32>>();
 
-    let mut elves_score = vec![];
-    let mut score = 0;
+    calories.sort_unstable();
 
-    for n in input {
-        if n == 0 {
-            elves_score.push(score);
-            score = 0;
-        } else {
-            score += n;
-        }
-    }
-
-    elves_score.sort_unstable();
-    elves_score.reverse();
-
-    let mut count: u8 = 1;
-    let mut res = 0;
-    for score in elves_score {
-        if count <= 3 {
-            res += score;
-            count += 1;
-        } else {
-            break;
-        }
-    }
-
-    println!("Result {res}");
+    println!("{}", calories.into_iter().rev().take(3).sum::<u32>());
 }
