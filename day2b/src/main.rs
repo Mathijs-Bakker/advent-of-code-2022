e39fb62def845ebbf8a71a79d1821cee4e7edf83
mathijs-bakker <mathijs_bakker@me.com>
2022-12-06 19:17:52 +0100

    Add Day 2 part 2

---
+++ day2b/src/main.rs
@@ -0,0 +1,14 @@
+fn main() {
+    let solution = include_str!("../data.txt")
+        .lines()
+        .map(|l| {
+            (
+                (l.as_bytes()[0] - b'A') as i16,
+                (l.as_bytes()[2] - b'X') as i16,
+            )
+        })
+        .map(|(a, b)| 1 + b + 3 * ((1 + b - a).rem_euclid(3)))
+        .sum::<i16>();
+
+    println!("score {:?}", solution);
+}
