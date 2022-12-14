565319e47917d9a30ce7b4fd7408bb05fefbb61b
mathijs-bakker <mathijs_bakker@me.com>
2022-12-14 09:59:49 +0100

    Solution to day3 part one

--- day3a/src/main.rs
+++ day3a/src/main.rs
@@ -1,12 +1,37 @@
 fn main() {
     // Read data as bytes
-    let data = include_bytes!("../example-input.txt")
-        // Create lines by splitting the data by the 'newline' byte escape
+    let solution: i16 = include_bytes!("../input.txt")
+        // Create lines by splitting the data by the 'newline' byte escape:
         .split(|b| *b == b'\n')
-        // Split lines in halve (two compartments)
-        .map(|l| l.split_at(l.len() / 2));
+        // Split lines in half (the two rugsack compartments) => ([u8, u8, ... ],[u8, u8, ... ]):
+        .map(|l| l.split_at(l.len() / 2))
+        // Iterate over the two compartments:
+        .map(|(a, b)| {
+            // Iterate over the second one:
+            b.iter()
+                // Find double entries or short-circuit to false to advance:
+                .find(|b| a.contains(b))
+                // Calculate the current item priority value:
+                .map(|b| {
+                    // To illustrate:
+                    // b'a' == 97, b'c' == 99
+                    // When 'b' is equal to b'c',
+                    // we calculate it like:
+                    // b'c' - b'a' + 1.
+                    // Which results in the value of 3.
+                    if *b >= b'a' {
+                        // calculate and return priority value
+                        (b - b'a') as i16 + 1
+                    } else {
+                        // calculate and return priority value
+                        (b - b'A') as i16 + 27
+                    }
+                })
+                // Last next() value is always None, we unwrap it to a zero value:
+                .unwrap_or(0)
+        })
+        // Sum op the returned priority values:
+        .sum();
 
-    for f in data {
-        println!("f {:?}", f);
-    }
+    println!("Solution {:?}", solution);
 }
