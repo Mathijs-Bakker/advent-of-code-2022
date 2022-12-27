9f61cc6db6c7ccc414de8fe08852b1da550045a3
mathijs-bakker <mathijs_bakker@me.com>
2022-12-27 23:56:48 +0100

    Some refactoring

--- day5a/src/main.rs
+++ day5a/src/main.rs
@@ -1,122 +1,62 @@
 #![warn(clippy::all, clippy::pedantic)]
-//     [D]
-// [N] [C]
-// [Z] [M] [P]
-//  1   2   3
 
-// move 1 from 2 to 1
-// move 3 from 1 to 3
-// move 2 from 2 to 1
-// move 1 from 1 to 2
-//----------
-
-// example-input.txt has 3 stacks of crates:
-// const N_STACKS: usize = 3;
+// const N_STACKS: usize = 3; // example-input.txt
 const N_STACKS: usize = 9;
 
 fn main() {
-    let data = include_str!("../input.txt");
     // let data = include_str!("../example-input.txt");
+    let data = include_str!("../input.txt");
 
-    // Split data into two
-    // let (crates, moves) = data.split_at("\n\n");
-    // let (crates, moves) = data.split_at(49); // DEMO
+    // Split data into crates and movement instructions:
+    // let (crates, moves) = data.split_at(49); // example-input.txt
     let (crates, moves) = data.split_at(325);
 
     let crates = crates.lines().collect::<Vec<&str>>();
 
     let mut stacks: [Vec<char>; N_STACKS] = Default::default();
-    for i in 0..N_STACKS {
+    (0..N_STACKS).for_each(|i| {
         for l in &crates {
-            println!("line: {l}",);
             // Populate stacks with crate id's:
             stacks[i].push(
                 l.chars()
-                    // Calculate the rows to fetch:
+                    // Calculate total rows to fetch:
                     .nth((i * 3) + 1 + i)
-                    // Filter only alphabetic characters
-                    .filter(|c| c.is_ascii_alphabetic())
-                    // If 'None' or non-alphabetical then add a dash.
+                    // Filter by alphabetic characters:
+                    .filter(char::is_ascii_alphabetic)
+                    // If 'None' or non-alphabetical then add a unique placeholder.
                     .unwrap_or('-'),
             );
-            // Clean up the stacks by popping the dashes.
+            // Clean up the stacks by removing the unique placeholders.
             if stacks[i].last() == Some(&'-') {
                 stacks[i].pop();
             }
         }
-        // Reverse crates for easy movement:
+        // Reverse crates for sane movement:
         stacks[i].reverse();
-    }
+    });
 
-    println!(" ---- STACKS ---- ");
-    for l in &stacks {
-        println!("Stack {:?}", l);
-    }
-
-    // let directions = moves.lines().collect::<Vec<&str>>();
-
-    // let directions: Vec<(u8, u8, u8)> = Default::default();
-    // let mut directions: Vec<&str> = Default::default();
-    let mut directions: Vec<Vec<&str>> = Default::default();
+    // Get the crane movement directions:
+    let mut directions: Vec<Vec<&str>> = Vec::default();
 
     for l in moves.lines().collect::<Vec<&str>>() {
-        // println!("dir line {:?}", l);
-        // directions = l.split_whitespace().collect::<Vec<&str>>();
         let temp = l.split_whitespace().collect::<Vec<&str>>();
         directions.push(temp);
-        // println!("foo {:?}", directions);
     }
 
-    // for d in &directions {
-    //     println!("direction {:?}", d);
-    // }
-
-    // MOVEMENTS:::
-    //     [D]
-    // [N] [C]
-    // [Z] [M] [P]
-    //  1   2   3
-
-    // move 1 from 2 to 1
-    // move 3 from 1 to 3
-    // move 2 from 2 to 1
-    // move 1 from 1 to 2
-    //-------
-
-    println!(" ---- MOVEMENTS ---- ");
-
-    let mut solution: String = "".to_owned();
     for d in directions {
         let n_crates: usize = d[1].parse().unwrap();
         let origin: usize = d[3].parse::<usize>().unwrap() - 1;
         let destination: usize = d[5].parse::<usize>().unwrap() - 1;
 
-        println!("d {:?}", d);
-        println!(
-            "Move number of crates: {:?} from origin: {:?} to dest: {:?}",
-            n_crates, origin, destination
-        );
-
-        // ncrates: 1, origin: 2, dest: 1
-        // from stacks[1] (origin)
-        // ncrates = 1 (one iteration)
-
-        // for i in 0..stacks[origin - 1].len() - n_crates {
-        for i in 0..n_crates {
-            println!("----- ");
-            // println!("stacks[origin].len(): {:?}", stacks[origin].len());
-            // println!("crate nr {:?}", i);
-
-            let temp = stacks.clone();
-            let v = temp[origin].last().unwrap();
+        (0..n_crates).for_each(|i| {
+            let tmp = stacks.clone();
+            let v = tmp[origin].last().unwrap();
             stacks[origin].pop();
             stacks[destination].push(*v);
-        }
-
-        for l in &stacks {
-            println!("Stack {:?}", l);
-        }
+        });
     }
+
+    let mut solution: String = String::new();
     for s in &stacks {
         solution.push(*s.last().unwrap_or(&' '));
     }
