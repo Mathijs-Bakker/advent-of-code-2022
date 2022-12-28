69f60958f3f2bde7e66a046ab39735465af59006
mathijs-bakker <mathijs_bakker@me.com>
2022-12-28 10:58:35 +0100

    Solution part two

---
+++ day5b/src/main.rs
@@ -0,0 +1,70 @@
+// const N_STACKS: usize = 3; // example-input.txt
+const N_STACKS: usize = 9;
+
+fn main() {
+    // let data = include_str!("../example-input.txt");
+    let data = include_str!("../input.txt");
+
+    // Split data into crates and movement instructions:
+    // let (crates, moves) = data.split_at(49); // example-input.txt
+    let (crates, moves) = data.split_at(325);
+
+    let crates = crates.lines().collect::<Vec<&str>>();
+
+    let mut stacks: [Vec<char>; N_STACKS] = Default::default();
+    (0..N_STACKS).for_each(|i| {
+        for l in &crates {
+            // Populate stacks with crate id's:
+            stacks[i].push(
+                l.chars()
+                    // Calculate total rows to fetch:
+                    .nth((i * 3) + 1 + i)
+                    // Filter by alphabetic characters:
+                    .filter(char::is_ascii_alphabetic)
+                    // If 'None' or non-alphabetical then add a unique placeholder.
+                    .unwrap_or('-'),
+            );
+            // Clean up the stacks by removing the unique placeholders.
+            if stacks[i].last() == Some(&'-') {
+                stacks[i].pop();
+            }
+        }
+        // Reverse crates:
+        stacks[i].reverse();
+    });
+
+    // Handle the crane movement directions:
+    let mut directions: Vec<Vec<&str>> = Vec::default();
+
+    for l in moves.lines().collect::<Vec<&str>>() {
+        let temp = l.split_whitespace().collect::<Vec<&str>>();
+        directions.push(temp);
+    }
+
+    for d in directions {
+        let n_crates: usize = d[1].parse().unwrap();
+        let origin: usize = d[3].parse::<usize>().unwrap() - 1;
+        let destination: usize = d[5].parse::<usize>().unwrap() - 1;
+
+        let mut crates_to_move: Vec<char> = Vec::default();
+        (0..n_crates).for_each(|_| {
+            let s = stacks.clone();
+            let v = s[origin].last().unwrap();
+            stacks[origin].pop();
+            crates_to_move.push(*v);
+        });
+
+        crates_to_move.reverse();
+
+        for c in crates_to_move {
+            stacks[destination].push(c);
+        }
+    }
+
+    let mut solution: String = String::new();
+    for s in &stacks {
+        solution.push(*s.last().unwrap_or(&' '));
+    }
+
+    println!("Solution: {solution:?}");
+}
